b2a1c586ff0c9827373e631c877a90e00b4af967
Chingiz <chingiz-askarov@list.ru>
2021-03-01 00:52:53 +0300

    Commit number three

--- src/script.js
+++ src/script.js
@@ -2,13 +2,15 @@
 
 
 
-/* var leftBorderWidth = 1;
+/* Переменные
+ var leftBorderWidth = 1;
 let second = 2;
 const pi = 3.14;
 
+Вывод в консоль
 console.log(second); */
 
-console.log(4/0);
+/* console.log(4/0);
 
 console.log(9 * 'china');
 
@@ -24,7 +26,7 @@ console.log(persone['name']);
 let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
 
 console.log(arr[0]);
-
+ */
 //alert('Hellow World!');
 
 /* let answer = confirm('Are you here?');
@@ -38,7 +40,7 @@ console.log(answer); */
 console.log('arr' + '-object');
 console.log(4 + + '-object'); */
 
-let incr = 10,
+/* let incr = 10,
     decr = 10;
 
 
@@ -52,6 +54,29 @@ console.log('5' == 5);
 let isChecked = false,
     isClose = false;
 
-console.log(isChecked || !isClose);
+console.log(isChecked || !isClose); */
+
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
+
 
 
